c2a3f99d58a68858ee80a7e6488b3ab5c60cb6d5
gabrieldasilvadev <gabrieldasilvaprivado1@gmail.com>
2021-12-08 04:00:42 -0300

    Finaliza navbar + responsividade

--- src/js/header/header.js
+++ src/js/header/header.js
@@ -2,11 +2,13 @@ const hamburguer =  document.getElementById('hamburguer');
 const menu = document.querySelector('.menu');
 const hero = document.getElementById('hero');
 const line = document.querySelector('.line');
+const navbar = document.querySelector('.listNav');
 
 function toggle() {
   menu.classList.toggle('active');
   hero.classList.toggle('active');
   line.classList.toggle('active');
+  navbar.classList.toggle('active');
 };
 
 hamburguer.addEventListener('click', toggle)
