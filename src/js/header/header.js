1970d58ee736cf93e7acfcdb095de578a1575adf
gabrieldasilvadev <gabrieldasilvaprivado1@gmail.com>
2021-12-08 04:12:13 -0300

    Corrige bug na navbar responsiva + transicao css

--- src/js/header/header.js
+++ src/js/header/header.js
@@ -2,13 +2,15 @@ const hamburguer =  document.getElementById('hamburguer');
 const menu = document.querySelector('.menu');
 const hero = document.getElementById('hero');
 const line = document.querySelector('.line');
-const navbar = document.querySelector('.listNav');
+const navbar = document.querySelector('.navbar');
+const listNav = document.querySelector('.listNav');
 
 function toggle() {
   menu.classList.toggle('active');
   hero.classList.toggle('active');
   line.classList.toggle('active');
   navbar.classList.toggle('active');
+  listNav.classList.toggle('active');
 };
 
 hamburguer.addEventListener('click', toggle)
