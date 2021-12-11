b0fb57b5d285c2f39667b8b5a0dbb073d9168d04
gabrieldasilvadev <gabrieldasilvaprivado1@gmail.com>
2021-12-11 19:30:04 -0300

    menu hamburguer: hidden no body  overflow-y para nao dar scroll

--- src/js/header/header.js
+++ src/js/header/header.js
@@ -3,12 +3,14 @@ const menu = document.querySelector('.menu');
 const line = document.querySelector('.line');
 const navbar = document.querySelector('.navbar');
 const listNav = document.querySelector('.listNav');
+const body = document.querySelector('body');
 
 function toggle() {
   menu.classList.toggle('active');
   line.classList.toggle('active');
   navbar.classList.toggle('active');
   listNav.classList.toggle('active');
+  body.classList.toggle('active');
 };
 
 hamburguer.addEventListener('click', toggle)
